a0e5ea620889226126396f0ffbfe02bd7c8be6bf
Terry Phelps <tgphelps50@gmail.com>
2021-06-03 10:52:32 -0400

    Shoe methods look okay.

--- shoe.go
+++ shoe.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"math/rand"
+	"time"
 	// "fmt"
 )
 
@@ -9,6 +10,11 @@ const bj_random_seed = 314159
 
 var suit = [13]int8{2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11}
 var deck [52]int8
+
+// When the Shoe.shuffle method is called, we store a pointer to the show
+// in 'curShoe', so our 'swap' function can find it. (Crude.)
+
+var curShoe *Shoe
 
 func init() {
 	n := 0
@@ -38,17 +44,38 @@ func newShoe(decks int) *Shoe {
 	var s Shoe
 	// s.numDecks = decks
 	s.next = 0
-	s.repeatable = false
 	s.shoeSize = 52 * decks
 	for i := 0; i < decks; i++ {
 		for _, j := range deck {
 			s.cards = append(s.cards, j)
 		}
 	}
+	rand.Seed(bj_random_seed)
 	return &s
 }
 
-func (s *Shoe) setRepeatable() {
-	s.repeatable = true
-	rand.Seed(bj_random_seed)
+func (s *Shoe) randomize() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func (s *Shoe) shuffle() {
+	curShoe = s
+	rand.Shuffle(s.shoeSize, swap)
+}
+
+func (s *Shoe) remaining() int {
+	return s.shoeSize - s.next
+}
+
+func (s *Shoe) deal() int8 {
+	c := s.cards[s.next]
+	s.next += 1
+	return c
+}
+
+func swap(i, j int) {
+	s := *curShoe
+	temp := s.cards[i]
+	s.cards[i] = s.cards[j]
+	s.cards[j] = temp
 }
--- shoe_test.go
+++ shoe_test.go
@@ -6,9 +6,35 @@ import (
 )
 
 func TestShoe1(t *testing.T) {
+	var c int8
 	s := newShoe(2)
 	if s.shoeSize != 104 {
 		t.Error("Bad deck count")
 	}
+	s.shuffle()
 	fmt.Println(s.cards)
+	if s.remaining() != 2*52 {
+		t.Error("Bad remaining() 1")
+	}
+	c = s.deal()
+	fmt.Printf("deal: %d\n", c)
+	c = s.deal()
+	fmt.Printf("deal: %d\n", c)
+	c = s.deal()
+	fmt.Printf("deal: %d\n", c)
+	c = s.deal()
+	fmt.Printf("deal: %d\n", c)
+	if s.remaining() != 2*52-4 {
+		t.Error("Bad remaining() 2")
+	}
+	for i := 0; i < 100; i++ {
+		c = s.deal()
+	}
+	if s.remaining() != 0 {
+		t.Error("Bad remaining() 3")
+	}
+
+	// s.randomize()
+	// s.shuffle()
+	// fmt.Println(s.cards)
 }
